4c81eb1e02bf8ba9fd1843407903cc84082896a0
jsonnek <jtsonnek@students.unwsp.edu>
2025-10-22 21:55:26 -0500

    Wrote State capital quiz code in program_3.py

--- program_3.py
+++ program_3.py
@@ -1,4 +1,7 @@
+# Jonathan Sonnek
+# October 22 2025
 # Program #3: Capital Quiz
+
 # Write a program that creates a dictionary containing the U.S. states as keys, 
 # and their capitals as values.  
 # The program should then randomly quiz the user by displaying the name of a state 
@@ -6,3 +9,79 @@
 # The program should count of the number of correct and incorrect responses.  
 # (You could alternatively use another country and provinces, 
 # or countries of the world and capitals).
+import random
+
+# Creating Dictionary
+us_states_capitals = {
+    "Alabama": "Montgomery",
+    "Alaska": "Juneau",
+    "Arizona": "Phoenix",
+    "Arkansas": "Little Rock",
+    "California": "Sacramento",
+    "Colorado": "Denver",
+    "Connecticut": "Hartford",
+    "Delaware": "Dover",
+    "Florida": "Tallahassee",
+    "Georgia": "Atlanta",
+    "Hawaii": "Honolulu",
+    "Idaho": "Boise",
+    "Illinois": "Springfield",
+    "Indiana": "Indianapolis",
+    "Iowa": "Des Moines",
+    "Kansas": "Topeka",
+    "Kentucky": "Frankfort",
+    "Louisiana": "Baton Rouge",
+    "Maine": "Augusta",
+    "Maryland": "Annapolis",
+    "Massachusetts": "Boston",
+    "Michigan": "Lansing",
+    "Minnesota": "Saint Paul",
+    "Mississippi": "Jackson",
+    "Missouri": "Jefferson City",
+    "Montana": "Helena",
+    "Nebraska": "Lincoln",
+    "Nevada": "Carson City",
+    "New Hampshire": "Concord",
+    "New Jersey": "Trenton",
+    "New Mexico": "Santa Fe",
+    "New York": "Albany",
+    "North Carolina": "Raleigh",
+    "North Dakota": "Bismarck",
+    "Ohio": "Columbus",
+    "Oklahoma": "Oklahoma City",
+    "Oregon": "Salem",
+    "Pennsylvania": "Harrisburg",
+    "Rhode Island": "Providence",
+    "South Carolina": "Columbia",
+    "South Dakota": "Pierre",
+    "Tennessee": "Nashville",
+    "Texas": "Austin",
+    "Utah": "Salt Lake City",
+    "Vermont": "Montpelier",
+    "Virginia": "Richmond",
+    "Washington": "Olympia",
+    "West Virginia": "Charleston",
+    "Wisconsin": "Madison",
+    "Wyoming": "Cheyenne"
+}
+
+def state_capital_test(questions):
+    # Creating score variable
+    score = 0
+    for number in range(questions):
+        # Retrieve State and Capital
+        random_state = random.choice(list(us_states_capitals.keys()))
+        capital = us_states_capitals[random_state]
+        # Ask for answer
+        answer = input("What is the capital of " + random_state + " ?")
+        # Respond to answer
+        if answer.strip().lower() == capital.strip().lower():
+            print("Correct!")
+            score += 1
+        else:
+            print("Incorrect!")
+    return score
+num_questions = int(input("How many questions would you like? "))
+correct = state_capital_test(num_questions)
+score = correct/num_questions*100
+print("You answered " + str(correct) + " out of " + str(num_questions) + " questions correctly, with a final score of " + str(score), "%")
